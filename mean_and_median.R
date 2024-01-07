e65c98a3ebdd944bb75d4bc9f0a11e734201b80f
Saima Sharleen Islam <saimasharleen@users.noreply.github.com>
2024-01-07 12:26:36 +0100

    Update mean_and_median.R

--- mean_and_median.R
+++ mean_and_median.R
@@ -1,3 +1,4 @@
+library(dplyr)
 food_consumption <- read_csv('data/food_consumption.csv',show_col_types = FALSE)
 head(food_consumption, 5)
 # Filter for Belgium
@@ -15,3 +16,13 @@ median(belgium_consumption$consumption)
 # Calculate mean and median consumption in USA
 mean(usa_consumption$consumption)
 median(usa_consumption$consumption)
+# Filter food_consumption for rows with data about Belgium and the USA. Group the filtered data by country. 
+# Calculate the mean and median of the kilograms of food consumed per person per year in each country. Call these columns mean_consumption and median_consumption
+food_consumption %>%
+  # Filter for Belgium and USA
+  filter(country %in% c("Belgium", "USA")) %>%
+  # Group by country
+  group_by(country, consumption) %>%
+  # Get mean_consumption and median_consumption
+  summarize(mean_consumption = mean(consumption),
+      median_consumption = median(consumption))
